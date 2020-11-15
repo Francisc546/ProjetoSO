edf0a294316fe7ce09c1787e0a1c4750de53ada7
Francisc546 <49655530+Francisc546@users.noreply.github.com>
2020-11-15 14:57:21 +0000

    Add files via upload

---
+++ library.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define UNIXSTR_PATH "/tmp/so20212044718"
+#define UNIXDG_PATH "/tmp/s.unixdfddffdfdgx"
+#define UNIXDG_TMP "/tmp/dgXXXXXfdjhfdjhfdXX"
